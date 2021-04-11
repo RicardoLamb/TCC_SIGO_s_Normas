8a79ab52d22bb99abbbdd593234b77311e24795f
Ricardo <rickrdoaugusto@gmail.com>
2021-04-10 21:26:34 -0300

    update fonts.

---
+++ s_gestao_de_normas/SGestaodeNormas/IncluirNormas.asmx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace SGestaodeNormas
+{
+    /// <summary>
+    /// Summary description for IncluirNormas
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class IncluirNormas : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public DataTable ListarNormas()
+        {
+            SqlConnection con = new SqlConnection("data source=.;Integrated Security=Yes;Database=sigonormas");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Normas"); 
+            SqlDataAdapter sda = new SqlDataAdapter(); 
+            cmd.Connection = con;
+            sda.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            dt.TableName = "Normas";
+            sda.Fill(dt);
+            return dt;
+        }
+
+        [WebMethod]
+        public DataTable FiltrarNorma(string Sigla, string Codigo)
+        {
+            SqlConnection con = new SqlConnection("data source=.;Integrated Security=Yes;Database=sigonormas");
+            SqlCommand cmd = new SqlCommand("select * from normas where sigla = @sigla and codigo = @codigo");
+            cmd.Parameters.AddWithValue("@Sigla", Sigla);
+            cmd.Parameters.AddWithValue("@Codigo", Codigo);
+            SqlDataAdapter sda = new SqlDataAdapter();
+            cmd.Connection = con;
+            sda.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            dt.TableName = "Normas";
+            sda.Fill(dt);
+            return dt;
+        }
+
+        [WebMethod]
+        public void AdicionarNorma(string Sigla, string Codigo, string Descricao, string DataDefinicao, string DataVigencia, string Origem, string Categoria)
+        {
+            SqlConnection con = new SqlConnection("data source=.;Integrated Security=Yes;Database=sigonormas");
+            SqlCommand cmd = new SqlCommand("insert into normas values(@Sigla, @Codigo, @Descricao, @DataDefinicao, @DataVigencia, @Origem, @Categoria)");
+            cmd.Parameters.AddWithValue("@Sigla", Sigla);
+            cmd.Parameters.AddWithValue("@Codigo", Codigo);
+            cmd.Parameters.AddWithValue("@Descricao", Descricao);
+            cmd.Parameters.AddWithValue("@DataDefinicao", DataDefinicao);
+            cmd.Parameters.AddWithValue("@DataVigencia", DataVigencia);
+            cmd.Parameters.AddWithValue("@Origem", Origem);
+            cmd.Parameters.AddWithValue("@Categoria", Categoria);
+            cmd.Connection = con;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            this.InsertSIGO(Sigla, Codigo, Descricao, DataDefinicao, DataVigencia, Origem, Categoria);
+            con.Close();
+            this.FiltrarNorma(Sigla, Codigo);
+        }
+
+        public void InsertSIGO(string Sigla, string Codigo, string Descricao, string DataDefinicao, string DataVigencia, string Origem, string Categoria)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://boo42sr8f6.execute-api.us-west-2.amazonaws.com/abnt/abnt/id?"+Codigo);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream())) {
+                string json = new JavaScriptSerializer().Serialize(new {
+                    id = Codigo,
+                    categoria = Categoria,
+                    codigo = Codigo,
+                    datadefinicao = DataDefinicao,
+                    datavigencia = DataVigencia,
+                    descricao = Descricao,
+                    origem = Origem,
+                    sigla = Sigla
+                });
+
+                streamWriter.Write(json);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            using (var streamReader = new StreamReader(response.GetResponseStream())) {
+                var result = streamReader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ s_gestao_de_normas/SGestaodeNormas/Normas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SGestaodeNormas
+{
+    public class Normas
+    {
+        public string Ukey { get; set; }
+        public string Sigla { get; set; }
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string DataDefinicao { get; set; }
+        public string DataVigencia { get; set; }
+        public string Origem { get; set; }
+        public string Categoria { get; set; }
+
+        public bool AdicionarNorma(string Sigla, string Codigo, string Descricao, string DataDefinicao, string DataVigencia, string Origem, string Categoria)
+        {
+            return true;
+        }
+
+        public Normas FiltrarNorma(string Sigla, string Codigo)
+        {
+            var norma = new Normas() { Ukey = "UKEY1", Sigla = "ABNT NBR", Codigo = "15527", Descricao = "Água de chuva - Aproveitamento de coberturas em áreas urbanas para fins não potáveis - Requisitos ", DataDefinicao = "2007-09-24 00:00:00", DataVigencia = "2007-10-24 00:00:00", Origem = "http://licenciadorambiental.com.br//wp-content/uploads/2015/01/NBR-15.527-Aproveitamento-%C3%A1gua-da-chuva.pdf", Categoria = "Ambiental" };
+            return norma;
+        }
+        public List<Normas> ListarNormas()
+        {
+            List<Normas> normas = new List<Normas>();
+            normas.Add(new Normas() { Ukey = "UKEY1", Sigla = "ABNT NBR", Codigo = "15527", Descricao = "Água de chuva - Aproveitamento de coberturas em áreas urbanas para fins não potáveis - Requisitos ", DataDefinicao = "2007-09-24 00:00:00", DataVigencia = "2007-10-24 00:00:00", Origem = "http://licenciadorambiental.com.br//wp-content/uploads/2015/01/NBR-15.527-Aproveitamento-%C3%A1gua-da-chuva.pdf", Categoria = "Ambiental" });
+            normas.Add(new Normas() { Ukey = "UKEY2", Sigla = "ABNT NBR", Codigo = "15112", Descricao = "Resíduos da construção civil e resíduos volumosos - Áreas de transbordo e triagem - Diretrizes para projeto, implantação e operação.", DataDefinicao = "2004-06-30 00:00:00", DataVigencia = "2004-07-30 00:00:00", Origem = "http://licenciadorambiental.com.br//wp-content/uploads/2015/01/NBR-15.112-RCC-e-Res%C3%ADduos-Volumosos.pdf", Categoria = "Ambiental" });
+            normas.Add(new Normas() { Ukey = "UKEY3", Sigla = "ABNT NBR", Codigo = "15575-5_2013", Descricao = "Edificações habitacionais — Desempenho - Parte 5: Requisitos para sistemas de coberturas", DataDefinicao = "2007-09-28 00:00:00", DataVigencia = "2007-11-27 00:00:00", Origem = "http://licenciadorambiental.com.br//wp-content/uploads/2015/01/NBR-15.575-5-Sistemas-de-Cobertura.pdf", Categoria = "Ambiental" });
+            return normas;
+        }
+    }
+}
